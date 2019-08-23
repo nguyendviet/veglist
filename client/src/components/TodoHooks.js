5925702dd938dc48d5fe4de61b6924a137790a8c
Viet Nguyen <vietjul@gmail.com>
2019-08-23 09:14:17 -0400

    style: todohooks

--- client/src/components/TodoHooks.js
+++ client/src/components/TodoHooks.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import {Checkbox, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, TextField} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -8,11 +7,11 @@ const useInputState = () => {
     const [value, setValue] = useState('');
   
     return {
-      value,
-      onChange: event => {
-        setValue(event.target.value);
-      },
-      reset: () => setValue('')
+        value,
+        onChange: event => {
+            setValue(event.target.value);
+        },
+        reset: () => setValue('')
     };
 };
 
@@ -20,84 +19,83 @@ const useTodoState = (initialValue) => {
     const [todos, setTodos] = useState(initialValue);
   
     return {
-      todos,
-      addTodo: todoText => {
-        setTodos([...todos, todoText]);
-      },
-      deleteTodo: todoIndex => {
-        const newTodos = todos.filter((_, index) => index !== todoIndex);
-  
-        setTodos(newTodos);
-      }
+        todos,
+        addTodo: todoText => {
+            setTodos([...todos, todoText]);
+        },
+        deleteTodo: todoIndex => {
+            const newTodos = todos.filter((_, index) => index !== todoIndex);
+            setTodos(newTodos);
+        }
     };
 };
 
 const TodoForm = ({ saveTodo }) => {
-  const { value, reset, onChange } = useInputState();
+    const { value, reset, onChange } = useInputState();
 
-  return (
-    <form
-      onSubmit={event => {
-        event.preventDefault();
+    return (
+        <form
+        onSubmit={event => {
+            event.preventDefault();
 
-        saveTodo(value);
-        reset();
-      }}
-    >
-      <TextField
-        variant="outlined"
-        placeholder="Add todo"
-        margin="normal"
-        onChange={onChange}
-        value={value}
-      />
-    </form>
-  );
+            saveTodo(value);
+            reset();
+        }}
+        >
+        <TextField
+            variant="outlined"
+            placeholder="Add item"
+            margin="dense"
+            onChange={onChange}
+            value={value}
+        />
+        </form>
+    );
 };
 
 const TodoList = ({ todos, deleteTodo }) => (
-  <List>
-    {todos.map((todo, index) => (
-      <ListItem key={index.toString()} dense button>
-        <Checkbox tabIndex={-1} disableRipple />
-        <ListItemText primary={todo} />
-        <ListItemSecondaryAction>
-          <IconButton
-            aria-label="Delete"
-            onClick={() => {
-              deleteTodo(index);
-            }}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
-    ))}
-  </List>
+    <List>
+        {todos.map((todo, index) => (
+        <ListItem key={index.toString()} dense button>
+            <Checkbox tabIndex={-1} disableRipple />
+            <ListItemText primary={todo} />
+            <ListItemSecondaryAction>
+            <IconButton
+                aria-label="Delete"
+                onClick={() => {
+                deleteTodo(index);
+                }}
+            >
+                <DeleteIcon />
+            </IconButton>
+            </ListItemSecondaryAction>
+        </ListItem>
+        ))}
+    </List>
 );
 
 const TodoHooksApp = () => {
-  const { todos, addTodo, deleteTodo } = useTodoState([]);
+    const { todos, addTodo, deleteTodo } = useTodoState([]);
 
-  return (
-    <div className="TodoHooksApp">
-      <Typography component="h1" variant="h2">
-        Todos
-      </Typography>
+    return (
+        <div className="TodoHooksApp">
+        <Typography component="h1" variant="h2">
+            VegList
+        </Typography>
 
-      <TodoForm
-        saveTodo={todoText => {
-          const trimmedText = todoText.trim();
+        <TodoForm
+            saveTodo={todoText => {
+            const trimmedText = todoText.trim();
 
-          if (trimmedText.length > 0) {
-            addTodo(trimmedText);
-          }
-        }}
-      />
+            if (trimmedText.length > 0) {
+                addTodo(trimmedText);
+            }
+            }}
+        />
 
-      <TodoList todos={todos} deleteTodo={deleteTodo} />
-    </div>
-  );
+        <TodoList todos={todos} deleteTodo={deleteTodo} />
+        </div>
+    );
 };
 
 export {TodoHooksApp};
