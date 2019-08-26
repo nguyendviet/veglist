648d021974af008a4b4bb106ddda3cdb3c7d7159
Viet Nguyen <vietjul@gmail.com>
2019-08-26 12:06:16 -0400

    feat: update pipeline. pipeline works

--- scripts/create-pipeline.js
+++ scripts/create-pipeline.js
@@ -1,7 +1,7 @@
 const {createStack} = require('../helpers/stacks');
 const {appName, pipelineStack, pipelineParams, templateBucketName, testBucketFolder} = require('../config/config');
 
-createStack(`test-${appName}-pipeline`, pipelineStack, pipelineParams, templateBucketName, testBucketFolder)
+createStack(`test-${appName}`, pipelineStack, pipelineParams, templateBucketName, testBucketFolder)
 .then((created) => {
     console.log(created);
     console.log('Stack created.');
