6d8b87949c7c7a7028fdf6b63df71d34faf2e55c
Viet Nguyen <vietjul@gmail.com>
2019-08-28 14:54:46 -0400

    test: clean bucket works

--- test/bucket-test.js
+++ test/bucket-test.js
@@ -7,27 +7,67 @@ const expect = chai.expect;
 process.env.AWS_SDK_LOAD_CONFIG=1;
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const BUCKET = "veglist-test-7d54aa10-2cd8-4c39-a4-pipelinebucket-13pepu4z4um6y";
 
-const myFuncPromise = async() => {
-    const params = {
-        Bucket: "veglist-test-edb8f258-f5b9-48b5-bf87-3fd21f3c318e-website"
+/**
+ * Function delete all objects inside a bucket,
+ * based on objects and bucket name.
+ * @param {obj} obj Objects to be deleted.
+ * @param {bucket} bucket Bucket name.
+ */
+const deleteThis = (obj, bucket) => {
+    var params = {
+        Bucket: bucket, 
+        Delete: {
+            Objects: obj,
+            Quiet: false
+        }
     };
 
-    s3.listObjectsV2(params).promise()
-    .then((res) => {
-        const objects = res.Contents;
-        const objKeys = objects.map((object) => {
-            return object.Key;
-        });
-
-        console.log(objKeys);
+    return new Promise((ressolve, reject) => {
+        s3.deleteObjects(params).promise()
+        .then((deleted) => {
+            console.log(`Bucket ${bucket} is now empty.`);
+            ressolve(deleted);
+        })
+        .catch((err) => console.log(err));
     })
-    .catch((err) => console.log(err));
-    return 'something';
 };
 
-describe('myFuncPromise', () => {
-    it('should eventually return string something', () => {
-        expect(myFuncPromise()).eventually.to.equal('something');
+/**
+ * Function remove all objects inside a bucket,
+ * based on the bucket name.
+ * @param {bucket} bucket Bucket name.
+ */
+const cleanThis = async(bucket) => {
+    const params = {
+        Bucket: bucket
+    };
+
+    return new Promise((resolve, reject) => {
+        // Get list of object keys.
+        s3.listObjectsV2(params).promise()
+        .then((res) => {
+            const objects = res.Contents;
+    
+            if (objects.length === 0) {
+                console.log(`Bucket ${bucket} is empty.`);
+            }
+            else {
+                console.log(`Deleting objects in bucket ${bucket}.`);
+                const keyObj = objects.map((object) => {
+                    return {Key: object.Key};
+                });
+                // Start deleting objects.
+                resolve(deleteThis(keyObj, bucket));
+            }
+        })
+        .catch((err) => console.log(err));
+    });
+};
+
+describe('cleanThis', () => {
+    it('should eventually return confirmation Deleted', () => {
+        expect(cleanThis(BUCKET)).eventually.to.have.property('Deleted');
     });
 });
