cbe34d7341d690cb15c2fe434dd25bba0c3711b1
Viet Nguyen <vietjul@gmail.com>
2019-09-06 17:17:35 -0400

    feat: add placeholder for select component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,127 +1,5 @@
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Input from '@material-ui/core/Input';
-import TextField from '@material-ui/core/TextField';
-// import MenuItem from '@material-ui/core/MenuItem';
-
-// const units = [
-//     {
-//       value: 'box',
-//       label: 'box',
-//     },
-//     {
-//       value: 'ounce',
-//       label: 'oz',
-//     }
-//   ];
-
-// const stores = [
-//     {
-//         value: 'giant',
-//         label: 'Giant',
-//     },
-//     {
-//         value: 'whole-foods',
-//         label: 'Whole Foods',
-//     }
-// ];
-  
-
-// const useStyles = makeStyles(theme => ({
-//     container: {
-//         display: 'flex',
-//         flexWrap: 'wrap',
-//       },
-//       textField: {
-//         marginLeft: theme.spacing(1),
-//         marginRight: theme.spacing(1),
-//       },
-//       dense: {
-//         marginTop: theme.spacing(2),
-//       },
-//       menu: {
-//         width: 200,
-//       },
-// }));
-
-// export default function Inputs() {
-//   const classes = useStyles();
-//   const [values, setValues] = React.useState({
-//     quantity: '',
-//     multiline: 'Controlled',
-//     unit: 'box',
-//   });
-
-//   const handleChange = name => event => {
-//     setValues({ ...values, [name]: event.target.value });
-//   };
-
-//   return (
-//     <form className={classes.container} noValidate autoComplete="off">
-//       <Input
-//         placeholder="Item"
-//         fullWidth
-//         className={classes.input}
-//         inputProps={{
-//           'aria-label': 'description',
-//         }}
-//       />
-//       <TextField
-//         id="number"
-//         placeholder="Quantity"
-//         value={values.quantity}
-//         onChange={handleChange('quantity')}
-//         type="number"
-//         className={classes.textField}
-//         InputLabelProps={{
-//           shrink: true,
-//         }}
-//         margin="normal"
-//       />
-//       <TextField
-//         id="select-quantity-native"
-//         select
-//         className={classes.textField}
-//         value={values.unit}
-//         onChange={handleChange('quantity')}
-//         SelectProps={{
-//           native: true,
-//           MenuProps: {
-//             className: classes.menu,
-//           },
-//         }}
-//         margin="normal"
-//       >
-//         {units.map(option => (
-//           <option key={option.value} value={option.value}>
-//             {option.label}
-//           </option>
-//         ))}
-//       </TextField>
-//       {/* <TextField
-//         id="select-store"
-//         select
-//         className={classes.textField}
-//         value={values.store}
-//         onChange={handleChange('store')}
-//         SelectProps={{
-//           MenuProps: {
-//             className: classes.menu,
-//           },
-//         }}
-//         margin="normal"
-//       >
-//         {stores.map(option => (
-//           <MenuItem key={option.value} value={option.value}>
-//             {option.label}
-//           </MenuItem>
-//         ))}
-//       </TextField> */}
-//     </form>
-//   );
-// }
-
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
@@ -177,35 +55,33 @@ const units = [
 const stores = [
     'Giant',
     'Whole Foods'
-]
+];
 
 export default function MultipleSelect() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [values, setValues] = React.useState({
-    quantity: '',
-    multiline: 'Controlled',
-    units: '',
-    stores: ''
-  });
+    const [values, setValues] = React.useState({
+        quantity: '',
+        units: 'Unit',
+        stores: 'Store'
+    });
 
     const handleChange = name => event => {
-        console.log({...values});
-        console.log(event.target.value);
-        setValues({ ...values, [name]: event.target.value });
+        const val = event.target.value
+        setValues({ ...values, [name]: val });
     };
 
     return (
         <div className={classes.root}>
             <FormControl className={classes.formControl}>
                 <Input
-                placeholder="Item"
-                fullWidth
-                className={classes.input}
-                inputProps={{
-                'aria-label': 'description',
-                }}
-            />
+                    placeholder="Item"
+                    fullWidth
+                    className={classes.input}
+                    inputProps={{
+                    'aria-label': 'description',
+                    }}
+                />
             </FormControl>
 
             <FormControl>
@@ -220,39 +96,39 @@ export default function MultipleSelect() {
                     shrink: true,
                     }}
                     margin="normal"
+                    fullWidth
                 />
             </FormControl>
             
             <FormControl className={clsx(classes.formControl, classes.noLabel)}>
                 <Select
                     value={values.units}
-                    name="units"
                     onChange={handleChange('units')}
                     autoWidth
-                    renderValue={selected => {
-                        if (selected === '') return <em>Unit</em>;
-                        else return selected;
+                    renderValue={(selected) => {
+                        return selected;
                     }}
                 >
-                    {/* TODO: Add default value from the array */}
-                    <MenuItem value="" disabled>
-                        Placeholder
-                    </MenuItem>
                     {units.map((unit) => (
-                        <MenuItem key={unit} value={unit}>{unit}</MenuItem>
+                        <MenuItem key={unit} value={unit}>
+                            {unit}
+                        </MenuItem>
                     ))}
                 </Select>
             </FormControl>
 
             <FormControl className={clsx(classes.formControl, classes.noLabel)}>
                 <Select
-                    displayEmpty
                     value={values.stores}
                     onChange={handleChange('stores')}
+                    renderValue={(selected) => {
+                        return selected;
+                    }}
+                    autoWidth
                 >
-                    {stores.map(store => (
+                    {stores.map((store) => (
                         <MenuItem key={store} value={store}>
-                        {store}
+                            {store}
                         </MenuItem>
                     ))}
                 </Select>
