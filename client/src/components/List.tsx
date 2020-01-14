13eebfb367868b40443578b894c967ab720c2f1a
Viet Nguyen <vietjul@gmail.com>
2020-01-14 16:08:06 -0500

    feat: list with check box. next: toggle slected

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,7 +7,8 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
+import CommentIcon from '@material-ui/icons/Comment';
+import { CircularProgress } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,6 +20,130 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+export default function CheckboxList() {
+    let initialGroceries = [
+        {
+            id: "one",
+            text: "Apple",
+            purchased: false
+        },
+        {
+            id: "two",
+            text: "Banana",
+            purchased: false
+        }
+    ];
+    const [groceries, setGroceries] = useState(initialGroceries);
+
+    const handlePurchase = (id: string) => () => {
+        const updatedGroceries = groceries.map((item) => {
+            if (item.id === id) {
+                if (item.purchased === false) item.purchased = true;
+                // if (item.purchased === true) item.purchased = false;
+            }
+            return item;
+        });
+        setGroceries(updatedGroceries);   
+    }
+
+    return (
+      <List>
+        {groceries.map((item) => {
+          const labelId = `checkbox-list-label-${item.id}`;
+  
+          return (
+            <ListItem key={item.id} role={undefined} dense button onClick={handlePurchase(item.id)}>
+              <ListItemIcon>
+                <Checkbox
+                  checked={item.purchased}
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={item.text} />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="comments">
+                  <CommentIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+}
+
+// export default function CheckboxList() {
+//     const classes = useStyles();
+//     const [checked, setChecked] = React.useState([-1]);
+  
+//     const handleToggle = (value: number) => () => {
+//       const currentIndex = checked.indexOf(value);
+//       const newChecked = [...checked];
+  
+//       if (currentIndex === -1) {
+//         newChecked.push(value);
+//       } else {
+//         newChecked.splice(currentIndex, 1);
+//       }
+  
+//       console.log(newChecked);
+//       setChecked(newChecked);
+//     };
+
+//     let initialGroceries = [
+//         {
+//             id: "one",
+//             text: "Apple",
+//             purchased: false
+//         },
+//         {
+//             id: "two",
+//             text: "Banana",
+//             purchased: false
+//         }
+//     ];
+//     const [groceries, setGroceries] = useState(initialGroceries);
+
+//     const handlePurchase = (id: string) => () => {
+//         groceries.forEach((item) => {
+//             if (item.id === id) {
+//                 item.purchased = true;
+//             }
+//         });
+//         setGroceries(groceries);   
+//     }
+
+//     return (
+//       <List className={classes.root}>
+//         {groceries.map((item) => {
+//           const labelId = `checkbox-list-label-${item.id}`;
+  
+//           return (
+//             // <ListItem key={item.id} role={undefined} dense button onClick={handleToggle(value)}>
+//             <ListItem key={item.id} role={undefined} dense button onClick={handlePurchase(item.id)}>
+//               <ListItemIcon>
+//                 <Checkbox
+//                   edge="start"
+//                   checked={item.purchased}
+//                   tabIndex={-1}
+//                   disableRipple
+//                   inputProps={{ 'aria-labelledby': labelId }}
+//                 />
+//               </ListItemIcon>
+//               <ListItemText id={labelId} primary={item.text} />
+//               <ListItemSecondaryAction>
+//                 <IconButton edge="end" aria-label="comments">
+//                   <CommentIcon />
+//                 </IconButton>
+//               </ListItemSecondaryAction>
+//             </ListItem>
+//           );
+//         })}
+//       </List>
+//     );
+// }
+
+/*
 export default function CheckboxList() {
   const classes = useStyles();
   const [checked, setChecked] = React.useState([-1]);
@@ -33,6 +158,7 @@ export default function CheckboxList() {
       newChecked.splice(currentIndex, 1);
     }
 
+    console.log(newChecked);
     setChecked(newChecked);
   };
 
@@ -55,7 +181,7 @@ export default function CheckboxList() {
             <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
             <ListItemSecondaryAction>
               <IconButton edge="end" aria-label="comments">
-                <DeleteIcon />
+                <CommentIcon />
               </IconButton>
             </ListItemSecondaryAction>
           </ListItem>
@@ -64,3 +190,4 @@ export default function CheckboxList() {
     </List>
   );
 }
+*/
