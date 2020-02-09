cbb0ebfd4d9833e05c4d8441768ba4f26003bbf3
Viet Nguyen <vietjul@gmail.com>
2020-02-09 09:22:31 -0500

    feat: form with submit working

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+
+import IconButton from '@material-ui/core/IconButton';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,25 +17,75 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+/**
+ * Custom hooks for input fields.
+ * @param initialState initialState for Input Fields
+ */
+function useFormFields(initialState: any) {
+    const [inputs, setValues] = useState(initialState);
+  
+    return [
+        inputs,
+        function(event: any) {
+            setValues({
+                ...inputs,
+                [event.target.id]: event.target.value
+            });
+        }
+    ];
+}
+
 export default function FormPropsTextFields() {
-  const classes = useStyles();
+    const classes = useStyles();
+    const [inputs, handleInputChange] = useFormFields({
+        item: '',
+        quantity: '',
+        store: ''
+    });
 
-  const [age, setAge] = React.useState('');
+    const handleSubmitItem = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log(inputs);
+    };
 
-  return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField required id="standard-required" label="Item" defaultValue="" />
-        <TextField
-          id="standard-number"
-          label="Quantity"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="standard-search" label="Store" type="search" />
-      </div>
-    </form>
-  );
+    return (
+        <form 
+            className={classes.root} 
+            noValidate autoComplete="off"
+            onSubmit={handleSubmitItem}
+        >
+            <div>
+                <TextField 
+                    required id="item" 
+                    label="Item" 
+                    value={inputs.item}
+                    onChange={handleInputChange}
+                />
+                <TextField
+                    id="quantity"
+                    label="Quantity"
+                    type="number"
+                    value={inputs.quantity}
+                    onChange={handleInputChange}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                <TextField 
+                    id="store" 
+                    label="Store" 
+                    type="search"
+                    value={inputs.store}
+                    onChange={handleInputChange}
+                />
+                <IconButton 
+                    type="submit"
+                    color="primary" 
+                    aria-label="add to shopping cart"
+                >
+                    <AddShoppingCartIcon />
+                </IconButton>
+            </div>
+        </form>
+    );
 }
